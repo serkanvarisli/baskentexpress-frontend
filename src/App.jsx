d6eb5ad3fd35a7f1ab8c07ee2f7376f998907349
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-04-15 18:42:42 +0300

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import Header from './components/header'
-import Footer from './components/footer'
-import Container from './components/container'
-import './style.css'
-
+import Footer from './components/footer';
+import Container from './components/container';
+import './style.css';
 
 function App() {
-
-  return (
-    <div>
-      <Container/>
-      <Footer/>
-    </div>
-  )
+    return (
+        <div>
+            <Container />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
